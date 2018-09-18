ae527551afaa1f0d3faf24d3c2b22e74700f1e78
Darryl Lardizabal <dsteven12@gmail.com>
2018-09-18 23:29:50 +0000

    Created table schema for users. Modified app.js file to count total number of sample users and console.log the total.

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 var faker = require('faker');
 var mysql = require('mysql');
 
+// CONNECTING to mysql db
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'dlardizabal',
     database: 'join_us'
 });
 
-var q = 'SELECT CURDATE() AS DATA';
+// CREATING Queries
+var q = 'SELECT COUNT(*) AS total FROM users';
 connection.query(q, function(err, res, fields) {
     if(err) throw err;
-    console.log(res[0].DATA);
+    console.log(`The total number of users are: ${res[0].total}`);
 });
 
+// ENDING connection so it doesn't persist
 connection.end();
 
-var newUser = {
-    email: faker.internet.email(),
-    pastDate: faker.date.past(),
-    seperator: '============================'
-};
+// var newUser = {
+//     email: faker.internet.email(),
+//     pastDate: faker.date.past(),
+//     seperator: '============================'
+// };
 
-for(var i = 0; i < 3; i++) {
-    console.log(newUser.seperator);
-    console.log(newUser.email);
-    console.log(newUser.pastDate);
-}
+// for(var i = 0; i < 3; i++) {
+//     console.log(newUser.seperator);
+//     console.log(newUser.email);
+//     console.log(newUser.pastDate);
+// }
 
 
