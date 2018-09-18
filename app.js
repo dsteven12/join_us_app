0f1356a49f6db98e18e6e9f8176b3085f7e955bb
Darryl Lardizabal <dsteven12@gmail.com>
2018-09-18 23:10:29 +0000

    Installed and connected to mysql db. Use node.js to create and test queries to db.

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var faker = require('faker');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'dlardizabal',
+    database: 'join_us'
+});
+
+var q = 'SELECT CURDATE() AS DATA';
+connection.query(q, function(err, res, fields) {
+    if(err) throw err;
+    console.log(res[0].DATA);
+});
+
+connection.end();
 
 var newUser = {
     email: faker.internet.email(),
     pastDate: faker.date.past(),
-    seperator: '=========================='
+    seperator: '============================'
 };
 
 for(var i = 0; i < 3; i++) {
