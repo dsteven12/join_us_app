b312a8b93c071cfb7bb99f9ea9afca1703e92a54
Darryl Lardizabal <dsteven12@gmail.com>
2018-09-20 16:47:25 +0000

    Created two insert data statements: One query a time vs. bulk insertion.

--- app.js
+++ app.js
@@ -8,26 +8,51 @@ var connection = mysql.createConnection({
     database: 'join_us'
 });
 
-// CREATING Queries
-var q = 'SELECT COUNT(*) AS total FROM users';
-connection.query(q, function(err, res, fields) {
-    if(err) throw err;
-    console.log(`The total number of users are: ${res[0].total}`);
+// SELECTING DATA
+// var q = 'SELECT COUNT(*) AS total FROM users';
+// connection.query(q, function(err, res, fields) {
+//     if(err) throw err;
+//     console.log(`The total number of users are: ${res[0].total}`);
+// });
+
+// INSERTING DATA v1 (HARD CODING single user)
+// var q = 'INSERT INTO users (email) VALUES ("rusty_the_dog@gmail.com")';
+// connection.query(q, function(err, res, fields) {
+//     if(err) throw err;
+//     console.log(`The total number of users are: ${res[0].total}`);
+// });
+
+// INSERTING DATA v2 (ONE QUERY AT A TIME)
+// for(var i = 0; i < 500; i++) {
+//     var person = {
+//         email: faker.internet.email(),
+//         created_at: faker.date.past()
+//     };
+//     connection.query('INSERT INTO users SET ?', person, function(err, res) {
+//         if(err) throw err;
+//         console.log(res);
+//     });    
+// }
+
+// INSERTING DATA v3 (BULK INSERT)
+var data = [];
+for(var i = 0; i < 500; i++){
+    data.push([
+        faker.internet.email(),
+        faker.date.past()
+    ]);
+}
+ 
+var q = 'INSERT INTO users (email, created_at) VALUES ?';
+ 
+connection.query(q, [data], function(err, result) {
+    console.log(err);
+    console.log(result);
 });
+ 
+
 
 // ENDING connection so it doesn't persist
 connection.end();
-
-// var newUser = {
-//     email: faker.internet.email(),
-//     pastDate: faker.date.past(),
-//     seperator: '============================'
-// };
-
-// for(var i = 0; i < 3; i++) {
-//     console.log(newUser.seperator);
-//     console.log(newUser.email);
-//     console.log(newUser.pastDate);
-// }
 
 
